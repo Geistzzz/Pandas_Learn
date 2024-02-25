c721a601428f6dc523d229180c3939260c5c017f
Geist <1319119251@qq.com>
2024-02-25 23:02:33 +0800

    daily

--- 03_read_files.py
+++ 03_read_files.py
@@ -6,6 +6,15 @@
 import numpy as np
 import pandas as pd
 
+# pandas存储
 data = np.random.randint(0, 100, (100, 3))
 df = pd.DataFrame(data, columns=['Chinese', 'Math', 'English'])
 df.to_csv('output.csv', index=False)
+df.to_excel('output.xlsx', index=False) #
+df.to_json('ouput.json', orient="records")
+
+# pandas读取
+# df = pd.read_csv('output.csv')
+df = pd.read_json('ouput.json', orient='records')
+
+print(df)
