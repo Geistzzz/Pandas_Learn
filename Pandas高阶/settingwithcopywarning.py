e7e2000c4de98cb42972a4fcd061b43632469ef3
Geistzzz <lilizhangchi@gmail.com>
2024-03-05 21:31:00 +0800

    daily

---
+++ Pandas高阶/settingwithcopywarning.py
@@ -0,0 +1,23 @@
+# -*- coding = utf-8 -*-
+# @Time : 2024/3/4 21:29
+# @Author : Geist
+# @File ： settingwithcopywarning.py
+# @Software: PyCharm
+import datetime
+
+# pandas不允许在子视图上进行修改
+# 一步到位
+
+import pandas as pd
+
+import datetime as dt
+
+t1 = dt.datetime.now()
+
+t2 = dt.datetime.now()
+
+# a = dt.time.
+# b = dt.time
+#
+#
+# print(b-a)
